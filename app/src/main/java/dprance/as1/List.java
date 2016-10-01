6c7b9b3ae8aa4cf5201606653d6a456e88f7e703
dprance <dprance@ualberta.ca>
2016-10-01 11:18:56 -0600

    pls

---
+++ app/src/main/java/dprance/as1/List.java
@@ -0,0 +1,7 @@
+package dprance.as1;
+
+/**
+ * Created by Duncan on 10/1/2016.
+ */
+public class List {
+}
---
+++ app/src/main/java/dprance/as1/User.java
@@ -0,0 +1,9 @@
+package dprance.as1;
+
+/**
+ * Created by Duncan on 10/1/2016.
+ */
+public class User
+{
+    public String Name;
+}
